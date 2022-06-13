2796936d2821863180cf3359a552de0cf5aaa212
insolor <insolor@gmail.com>
2022-06-14 00:12:32 +0300

    Convert u16 pointer to a WString

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,53 @@
-use std::ptr::null;
+use pyo3::{prelude::*, types::PyBytes};
+use utf16string::{LE, WString};
+use std::{ptr::null};
 
 #[no_mangle]
 pub extern "C" fn Init() -> u64 {
+    pyo3::prepare_freethreaded_python();
+    
     return 0;
 }
 
+fn read_wstring(text: *const u16) -> Vec<u8> {
+    let mut vector: Vec<u8> = Vec::new();
+    
+    unsafe {
+        let mut idx: isize = 0;
+        while *text.offset(idx) != 0 {
+            let bytes = (*text.offset(idx)).to_le_bytes();
+            vector.push(bytes[0]);
+            vector.push(bytes[1]);
+            idx += 1;
+        } 
+    }
+    
+    vector
+}
+
 #[no_mangle]
-pub extern "C" fn Changetext(_text: *const u16) -> *const u16 {
+pub extern "C" fn Changetext(text: *const u16) -> *const u16 {
+    let py_app = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/changetext.py"));
+    
+    let text = WString::<LE>::from_utf16le(read_wstring(text)).expect("Conversion error");
+    
+    Python::with_gil(|py| -> PyResult<()> {
+        let changetext: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
+            .getattr("ChangeText")?
+            .into();
+        
+        // let text = WString::<LE>::from("hello");
+        
+        let py_bytes = PyBytes::new(py, text.as_bytes());
+        
+        let result = changetext.call1(py, (py_bytes,))?;
+        let result: Vec<u8> = result.extract(py)?;
+        
+        let result = WString::from_utf16le(result).expect("UTF-16 decode error");
+        let result = result.to_utf8();
+        
+        Ok(())
+    }).expect("Something went wrong");
+    
     return null();
 }
