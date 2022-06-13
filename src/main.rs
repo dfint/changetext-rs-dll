745e27d10a897c99c31130e2eb75c148200fefd6
insolor <insolor@gmail.com>
2022-06-13 17:38:46 +0300

    Change dependecy to rustpython-vm, try example code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rustpython_vm as vm;
+
+fn main() -> vm::PyResult<()> {
+    vm::Interpreter::without_stdlib(Default::default()).enter(|vm| {
+        let scope = vm.new_scope_with_builtins();
+
+        let code_obj = vm
+            .compile(
+                r#"print("Hello World!")"#,
+                vm::compile::Mode::Exec,
+                "<embedded>".to_owned(),
+            )
+            .map_err(|err| vm.new_syntax_error(&err))?;
+
+        vm.run_code_obj(code_obj, scope)?;
+
+        Ok(())
+    })
 }
